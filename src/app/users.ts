aaa551090b90a3a2432c9ed198282ce0c44e196f
Millen DwiPutra <millendwiputra20@gmail.com>
2020-05-09 01:09:57 +0700

    First Commit
    Udah jadi Home Page pake bootstrap
    udah bisa routing

---
+++ src/app/users.ts
@@ -0,0 +1,21 @@
+export const users = [
+    {
+      name: 'AA',
+      ID: 1,
+    },
+    {
+        name: 'BB',
+        ID: 2,
+    },
+    {
+        name: 'CC',
+        ID: 3,
+    }
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at http://angular.io/license
+  */
