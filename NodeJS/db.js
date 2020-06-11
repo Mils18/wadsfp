45200fbaf724d00ce284673b7419399c3dd4a1f8
Millen DwiPutra <millendwiputra20@gmail.com>
2020-06-11 22:45:43 +0700

    Update ip

--- NodeJS/db.js
+++ NodeJS/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/WADSFP', (err) => {
+mongoose.connect('mongodb://admin:admin123@52.77.251.157/WADSFP?authSource=admin', (err) => {
     if (!err)
         console.log('MongoDB connection succeded...');
     else
-        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+        console.log(err);
+        // console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
 })
 module.exports == mongoose;
